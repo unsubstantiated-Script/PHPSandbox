7335bd87e06b0f55661de2a7ba98bfcd9cfa60a6
Justin Fulton <jusfulton@gmail.com>
2021-03-31 09:37:42 -0700

    Basic Email Form Validtion and Setup

--- website3/index.php
+++ website3/index.php
@@ -1,3 +1,53 @@
+<?php
+
+//Message vars
+$msg = '';
+$msgClass = '';
+
+//Check for submit
+if (filter_has_var(INPUT_POST, 'submit')) {
+    //Get Form Data;
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    //Check required fields
+
+    if (!empty($name) && !empty($email) && !empty($message)) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $msg = 'Please use a valid email!';
+            $msgClass = 'alert-danger';
+        } else {
+            $toEmail = 'email@gmail.com';
+            $subject = 'Contact request from ' . $name;
+            $body = '<h2>Contact Request</h2>
+            <h4>Name</h4><p>' . $name . '</p>
+            <h4>Email</h4><p>' . $email . '</p>
+            <h4>Message</h4><p>' . $message . '</p>';
+
+            //Email Headers
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-Type:text/html;charset-UTF-8" . "\r\n";
+
+            //Additional headers...
+            $headers .= "From: " . $name . "<" . $email . ">" . "\r\n";
+
+            if (mail($toEmail, $subject, $body, $headers)) {
+                $msg = 'Your email has been sent';
+                $msgClass = 'alert-success';
+            } else {
+                $msg = 'Your email has not been sent...';
+                $msgClass = 'alert-danger';
+            }
+        }
+    } else {
+        $msg = 'Please fill in all fields';
+        $msgClass = 'alert-danger';
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,18 +69,29 @@
     </nav>
 
     <div class="container">
-        <form action="" method="post" class="mt-2">
+
+
+        <?php if ($msg != ''): ?>
+        <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?>
+        </div>
+        <?php endif;?>
+
+
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="mt-2">
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" name="name" class="form-control" value="">
+                <input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>
+">
             </div>
             <div class="form-group">
                 <label>Email</label>
-                <input type="text" name="email" class="form-control" value="">
+                <input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>
+">
             </div>
             <div class="form-group">
                 <label>Message</label>
-                <textarea name="message" class="form-control" value=""></textarea>
+                <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?>
+                </textarea>
             </div>
             <br>
             <button class="btn btn-primary" type="submit" name="submit">Submit</button>
